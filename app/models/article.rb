ec078baeb6b29beac4d39b3884fc71c26cc0ce66
lollichilli <nguyth02@luther.edu>
2023-06-12 15:49:32 +0700

    Almost finished except Destroy feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,17 @@
+# Validations & Displaying Error Message
+# This is useful for validating invalid users' inputs
+
 class Article < ApplicationRecord
+
+    # This enable a good bit of automatic behaviour
+    # For example, if you have an instance variable @article containing an article, you can 
+    # retrieve all the comments belonging to that article as an array using @article.comments.
+    has_many :comments
+
+    # Title must be present. It must at least contain 1 non-whitespace character
+    validates :title, presence: true
+
+    # Body must be present. Also, It should be at least 10 characters long
+    validates :body, presence: true, length: { minimum: 10 }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,19 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 
-  get "/articles", to: "articles#index"
+  
+  # get "/articles", to: "articles#index"
 
+  # By having an extra path :id, this designates a route parameter. A route parameter 
+  # captures a segment of the request's path, and puts that value into the params Hash,
+  # which is accessible by the controller action. For example, when handling a request 
+  # like GET http://localhost:3000/articles/1, 1 would be captured as the value for :id,
+  # which would then be accessible as params[:id] in the show action of ArticlesController
 
+  # get "/articles/:id", to: "articles#show"
+
+  # Replace the two get routes in config/routes.rb with resources
+  resources :articles do
+    resources :comments
+  end
 end
